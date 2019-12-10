9ef5a62ed6c44b341b457b981232d9f436e3c1e2
Jonathan Palma <jonathanpalma89@gmail.com>
2019-12-10 16:05:40 +0000

    Refactor FizzBuzz.

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,16 +1,8 @@
 var Fizzbuzz = function() {};
 
 Fizzbuzz.prototype.number = function(value) {
-
-    if(value % 15 == 0) {
-        return 'fizzbuzz';
-    };    
     
-    if(value % 3 == 0) {
-        return 'fizz';
-    };
-    if(value % 5 == 0) {
-        return 'buzz';
-    };
-    
+    if(value % 15 == 0) { return 'fizzbuzz'; };    
+    if(value % 3 == 0) { return 'fizz'; };
+    if(value % 5 == 0) { return 'buzz'; };
 };
