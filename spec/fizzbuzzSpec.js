aff233e2d78245525532eba0285423725c18d7e6
Student <student@Makerss-MacBook-Pro.local>
2019-12-10 15:53:56 +0000

    Fizzbuzz Javascript

---
+++ spec/fizzbuzzSpec.js
@@ -0,0 +1,23 @@
+
+
+ 
+
+describe('Fizzbuzz', function() {
+
+    var  fizzbuzz;
+    it("should be divisible by 3", function() {
+    fizzbuzz = new Fizzbuzz();    
+    expect(fizzbuzz.number(3)).toEqual('fizz');
+    });
+
+    it("should be divisible by 5", function() {
+    fizzbuzz = new Fizzbuzz();
+    expect(fizzbuzz.number(5)).toEqual('buzz');
+    });
+
+    it("should be divisible by 15", function() {
+        fizzbuzz = new Fizzbuzz();
+        expect(fizzbuzz.number(15)).toEqual('fizzbuzz');
+        });
+});
+
---
+++ src/fizzbuzz.js
@@ -0,0 +1,16 @@
+var Fizzbuzz = function() {};
+
+Fizzbuzz.prototype.number = function(value) {
+
+    if(value % 15 == 0) {
+        return 'fizzbuzz';
+    };    
+    
+    if(value % 3 == 0) {
+        return 'fizz';
+    };
+    if(value % 5 == 0) {
+        return 'buzz';
+    };
+    
+};
