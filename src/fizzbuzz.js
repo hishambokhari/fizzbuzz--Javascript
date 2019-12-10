b069067c48159d0601d09333c5214bb593309ec4
Jonathan Palma <jonathanpalma89@gmail.com>
2019-12-10 16:47:31 +0000

    add return value if not fizz &&|| buzz.

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,9 @@
 var Fizzbuzz = function() {};
 
 Fizzbuzz.prototype.number = function(value) {
-    
+
     if(value % 15 == 0) { return 'fizzbuzz'; };    
     if(value % 3 == 0) { return 'fizz'; };
     if(value % 5 == 0) { return 'buzz'; };
+    return value;
 };
